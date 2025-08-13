86035fbed7301fbe09292b280446590fe6a252ca
iamshubham1704 <sstech514@gmail.com>
2025-08-14 01:11:22 +0530

    notification

---
+++ app/seller-dashboard/notifications/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const notifications = () => {
+  return (
+    <div>Under development...........Kya krna hai notification ka website kholke ke check krle</div>
+  )
+}
+
+export default notifications
