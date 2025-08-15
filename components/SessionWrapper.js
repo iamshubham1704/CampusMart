7e24f36ce3c4e86bdae1cfb7475c91a15c95eb7e
Shubham-Raj06 <122121462+Shubham-Raj06@users.noreply.github.com>
2025-08-15 17:08:26 +0530

    home page change

--- components/SessionWrapper.js
+++ components/SessionWrapper.js
@@ -1,7 +1,6 @@
-"use client";
+'use client';
+import { SessionProvider } from 'next-auth/react';
 
-import { SessionProvider } from "next-auth/react";
-
-export default function SessionWrapper({ children, session }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+export default function SessionWrapper({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
